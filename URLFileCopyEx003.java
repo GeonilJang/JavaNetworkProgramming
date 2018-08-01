47ed0ca4fe05250e6e0f99bdd4826b66470be4c1
GeonilJang <jgi0105@naver.com>
2018-08-01 21:28:38 +0900

    02 openStream + BufferedInputStream + FileOutputStream 을 이용하여 해당 url의 이미지 파일을 복사하는 진행률 알아보기 위해서 URLConnection 객체 사용.

--- URLFileCopyEx003.java
+++ URLFileCopyEx003.java
@@ -2,6 +2,7 @@ import java.io.BufferedInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.net.URL;
+import java.net.URLConnection;
 
 public class URLFileCopyEx003 {
 	public static void main(String[] args) throws Exception {
@@ -15,9 +16,16 @@ public class URLFileCopyEx003 {
 		FileOutputStream fo = new FileOutputStream("D:\\test3.jpg");
 		byte buff[] = new byte[2048];
 		int imgData = 0;
-		while((imgData = bi.read(buff)) != -1) {
-			fo.write(buff, 0, imgData);
+		
+		int cnt = 0 ;
+		URLConnection conn = url.openConnection();
+		int size = conn.getContentLength();
+		System.out.println("이미지 크기: "+size);
+		while((imgData = bi.read(buff)) != -1) { //read(buff) -> buff에 값을 담는다.
+			fo.write(buff, 0, imgData);//imgData 에는 버퍼의 크기가 들어간다.
 			fo.flush();
+			cnt +=imgData;
+			System.out.println(((cnt*100)/size)+"%");
 		}
 		fo.close();
 		bi.close();
