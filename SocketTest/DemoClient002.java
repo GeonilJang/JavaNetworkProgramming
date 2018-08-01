006648899460db3ec90257a1fa7b3e9853da7430
GeonilJang <jgi0105@naver.com>
2018-08-01 22:51:25 +0900

    소켓을 이용해서 서버에서 값을 전송하기

--- SocketTest/DemoClient002.java
+++ SocketTest/DemoClient002.java
@@ -1,15 +1,28 @@
 package SocketTest;
 
-import java.io.IOException;
+import java.io.DataInputStream;
+import java.io.InputStream;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
 public class DemoClient002 {
 	public static void main(String[] args) throws Exception {
 		
 		//연결시에 소켓이 생성이 된다. 연결이 안될경우에는 예외 발생한다.
-		Socket sk = new Socket("183.99.6.38",5052);
+		Socket sk = new Socket("183.99.6.38",5049);
 		System.out.println("서버와 접속 : 5050");
+		
+		InputStream ins = sk.getInputStream();
+		DataInputStream dins = new DataInputStream(ins);
+		
+		int number = dins.readInt();
+		String str = dins.readUTF();
+		System.out.println("서버에서 전송된 값 : "+number+" , "+str);
+		dins.close();
+		ins.close();
+		sk.close();
+		
+		
+		
 		
 	}
 }
--- SocketTest/DemoServer001.java
+++ SocketTest/DemoServer001.java
@@ -1,26 +1,35 @@
 package SocketTest;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class DemoServer001 {
 	public static void main(String[] args) throws IOException {
 		//서버입니다.
-		int port = 5052;
+		int port = 5049;
+		
+		int number = Integer.parseInt(args[0]);
+		String str = new String(args[1]);
 		
 		//서버소켓 생성
-		ServerSocket ssk = new ServerSocket(port); //클라이언트가 접속했는지 안했는지만 판단을 합니다.
+		ServerSocket ssk = new ServerSocket(port); //클라이언트가 접속했는지 안했는지만 판단을 합니다.-> accept() 실행됨.
 		System.out.println("접속대기중.");
 		
 		while(true) {
 			Socket socket = ssk.accept();
 			System.out.println("클라이언트 ip : "+socket.getInetAddress().getHostAddress());
+			
+			//클라이언트와 연결을 위한 스트림을 생성.
+			OutputStream ous = socket.getOutputStream();
+			DataOutputStream dous = new DataOutputStream(ous);
+			dous.writeInt(number);
+			dous.writeUTF(str);
+			dous.flush();
+			dous.close();
+			socket.close();
 		}
-		
-		
-		
-		
-		
 	}
 }
